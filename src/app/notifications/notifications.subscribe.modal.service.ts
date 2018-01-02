4c6167ec5448cc2c34a284f32f304151b6b367ac
Apurv <apurv2@gmail.com>
2018-01-02 15:55:36 -0800

    fixed http build issue

--- src/app/notifications/notifications.subscribe.modal.service.ts
+++ src/app/notifications/notifications.subscribe.modal.service.ts
@@ -1,7 +1,8 @@
 import { Observable } from "rxjs/Observable";
 import { NotificationSettings } from "app/notifications/models/notification.settings.model";
 import { environment } from "environments/environment";
-import { Http } from "@angular/http/src/http";
+import { Http } from '@angular/http';
+
 import { Injectable } from "@angular/core";
 
 @Injectable()
