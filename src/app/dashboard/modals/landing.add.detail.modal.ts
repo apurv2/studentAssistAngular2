9a16e8bee93d0fc4eaa23fb05218ed5ca38ea4ea
apurv <apurv2@gmail.com>
2018-01-31 22:10:51 -0500

    popup on landing page

---
+++ src/app/dashboard/modals/landing.add.detail.modal.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core/src/metadata/directives";
+
+@Component({
+    selector: 'notification-settings',
+    templateUrl: 'notifications.subscribe.modal.html',
+})
+
+export class landingAddDetailModal {
+}
